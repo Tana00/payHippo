1d5dbe4cc5d41a8ba9f103c0a5756a382a1defa8
happiness-balogun <74186188+happiness-balogun@users.noreply.github.com>
2021-06-27 08:41:07 +0100

    onboarding pages successfully done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import PersonalProfile from "./pages/PersonalProfile";
+import Verification from "./pages/Verification";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route path="/profile">
+            <PersonalProfile />
+          </Route>
+          <Route path="/verify">
+            <Verification />
+          </Route>
+          <Route path="/">
+            <PersonalProfile />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
